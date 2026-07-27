cd8b535185ca9c30e424fcd13c9c2fb630397932
Brian2994 <brianpari29@gmail.com>
2026-07-27 12:14:57 -0300

    feat: implementa rolagem infinita para carregar todos os anuncios

--- main.py
+++ main.py
@@ -32,62 +32,78 @@ with sync_playwright() as p:
     
     print("Página carregada!")
 
-    cards = page.locator(".car")
+    print("\nCarregando todos os anúncios...")
+    quantidade_anterior = 0
+    while True:            
+        cards = page.locator(".car")
+        quantidade_atual = cards.count()            
+        print(f"Veículos carregados: {quantidade_atual}")
 
-    print(f"Foram encontrados {cards.count()} anúncios.")
+        if quantidade_atual == quantidade_anterior:
+            print("\nTodos os anúncios foram carregados!")
+            break
 
-    veiculos = []
+        quantidade_anterior = quantidade_atual
 
-    # extrair tudo do primeiro carro
-    for i in range(cards.count()):
-        carro = cards.nth(i)
-        print(f"\nProcessando veículo {i+1}/{cards.count()}")
+        page.mouse.wheel(0, 6000)
+        page.wait_for_timeout(2000)
 
-        titulo = obter_texto(carro.locator(".info__title"))
-        versao = obter_texto(carro.locator(".info__subtitle"))
-        dados = obter_texto(carro.locator(".add__info"))
+    # cards = page.locator(".car")
 
-        ano, quilometragem, cidade = [
-            item.strip()
-            for item in dados.split("•")
-        ]
+    # print(f"Foram encontrados {cards.count()} anúncios.")
 
-        preco_antigo = obter_texto(carro.locator(".price-24"))
-        preco_atual = obter_texto(carro.locator(".price-30"))
-        imagem = carro.locator(".vehicle-img").get_attribute("src")
-        link = carro.locator("a").get_attribute("href")
-        url_completo = BASE_URL + link
+    # veiculos = []
 
-        print("Título:", titulo)
-        print("Versão:", versao)
-        print("Dados:", dados)
-        print("Ano:", ano)
-        print("KM:", quilometragem)
-        print("Cidade:", cidade)
-        print("Preço antigo:", preco_antigo)
-        print("Preço atual:", preco_atual)
-        print("Imagem:", imagem)
-        print("Link:", url_completo)
+    # # extrair tudo do primeiro carro
+    # for i in range(cards.count()):
+    #     carro = cards.nth(i)
+    #     print(f"\nProcessando veículo {i+1}/{cards.count()}")
 
-        veiculo = {
-        "titulo": titulo,
-        "versao": versao,
-        "ano": ano,
-        "quilometragem": quilometragem,
-        "cidade": cidade,
-        "preco_antigo": preco_antigo,
-        "preco_atual": preco_atual,
-        "imagem": imagem,
-        "url": url_completo
-        }
+    #     titulo = obter_texto(carro.locator(".info__title"))
+    #     versao = obter_texto(carro.locator(".info__subtitle"))
+    #     dados = obter_texto(carro.locator(".add__info"))
 
-        veiculos.append(veiculo)
-        print(f"✓ {titulo}")
+    #     ano, quilometragem, cidade = [
+    #         item.strip()
+    #         for item in dados.split("•")
+    #     ]
 
-    print("\n=================================")
-    print(f"Total coletado: {len(veiculos)}")
-    print("\nPrimeiro veículo:")
-    print(veiculos[0])
+    #     preco_antigo = obter_texto(carro.locator(".price-24"))
+    #     preco_atual = obter_texto(carro.locator(".price-30"))
+    #     imagem = carro.locator(".vehicle-img").get_attribute("src")
+    #     link = carro.locator("a").get_attribute("href")
+    #     url_completo = BASE_URL + link
+
+    #     print("Título:", titulo)
+    #     print("Versão:", versao)
+    #     print("Dados:", dados)
+    #     print("Ano:", ano)
+    #     print("KM:", quilometragem)
+    #     print("Cidade:", cidade)
+    #     print("Preço antigo:", preco_antigo)
+    #     print("Preço atual:", preco_atual)
+    #     print("Imagem:", imagem)
+    #     print("Link:", url_completo)
+
+    #     veiculo = {
+    #     "titulo": titulo,
+    #     "versao": versao,
+    #     "ano": ano,
+    #     "quilometragem": quilometragem,
+    #     "cidade": cidade,
+    #     "preco_antigo": preco_antigo,
+    #     "preco_atual": preco_atual,
+    #     "imagem": imagem,
+    #     "url": url_completo
+    #     }
+
+    #     veiculos.append(veiculo)
+    #     print(f"✓ {titulo}")
+
+    # print("\n=================================")
+    # print(f"Total coletado: {len(veiculos)}")
+    # print("\nPrimeiro veículo:")
+    # print(veiculos[0])
 
     input("\nPressione ENTER para fechar...")
 
